1d409044a7a6c9634bb771c3ba6d1fede7389736
3DPrintScanner <1988anthonythomas@gmail.com>
2017-10-08 12:18:45 +0100

    add images and links

--- app/models/lighting_product.rb
+++ app/models/lighting_product.rb
@@ -2,4 +2,9 @@ class LightingProduct < ApplicationRecord
   has_and_belongs_to_many :countries
   has_and_belongs_to_many :distributors
   belongs_to :manufacturer
+
+
+  def update_image_url(url)
+  	update_attribute(:image_url, url)
+  end 
 end
